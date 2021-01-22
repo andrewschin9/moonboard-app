c8d4c9fecaf67b7949be178269e69c759086d43c
Andrew Chin <andrewschin9@gmail.com>
2021-01-21 18:43:48 -0600

    start ratings feature

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,4 +2,5 @@ class Problem < ApplicationRecord
   has_many :favorites
   has_many :picked_holds
   validates :prob_name, presence: true 
+  has_many :ratings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_035009) do
+ActiveRecord::Schema.define(version: 2021_01_22_004100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2021_01_08_035009) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "hold_set_id"
     t.integer "user_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "problem_id"
+    t.integer "user_id"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
